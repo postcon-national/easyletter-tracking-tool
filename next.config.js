c9d5186de77b92cb78bbbf129db7b74d8e3c36e3
Thomas Wiens <thomas.wiens@mindcaptain.de>
2025-01-10 12:13:43 -0600

    Update Dockerfile and Next.js config
    - Changed npm install command to use `--legacy-peer-deps`.
    - Added `outputFileTracing` and `experimental.outputStandalone` in Next.js config.
    - Set `distDir` to '.next' for custom build directory.

--- next.config.js
+++ next.config.js
@@ -1,26 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: {
+output: 'standalone',
+outputFileTracing: true,
+experimental: {
+    outputStandalone: true,
+},
+distDir: '.next',
+eslint: {
     ignoreDuringBuilds: true,
-  },
-  output: "standalone",
-  images: {
+},
+images: {
     remotePatterns: [
-      {
+    {
         protocol: "https",
         hostname: "encrypted-tbn0.gstatic.com",
         port: "",
         pathname: "/**",
-      },
-      {
+    },
+    {
         protocol: "https",
         hostname: "www.deutscherversandservice.de",
         port: "",
         pathname: "/**",
-      },
+    },
     ],
-  },
+},
 };
 
 module.exports = nextConfig;
